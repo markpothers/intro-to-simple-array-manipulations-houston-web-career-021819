69e10169974b1c1d43c35a14d604106316e5d9cc
Mark Pothecary <markpothers@hotmail.com>
2019-02-10 16:02:24 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args
+
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args
+
+end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, element)
+  array.insert(element)
+end
+
+def using_uniq
+
+end
+
+def using_flatten
+
+end
+
+def using_delete
+
+end
+
+def using_delete_at
+
+end
