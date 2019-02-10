6358f088b756e47238e4503191085043ecf539bb
Mark Pothecary <markpothers@hotmail.com>
2019-02-10 16:08:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,16 +10,16 @@ def using_pop(array)
   array.pop
 end
 
-def pop_with_args
-
+def pop_with_args(array)
+  array.pop(2)
 end
 
 def using_shift(array)
   array.shift
 end
 
-def shift_with_args
-
+def shift_with_args(array)
+  array.shift(2)
 end
 
 def using_concat(array1, array2)
